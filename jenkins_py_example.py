9fe8b3c05adef605d75b814f782bc79e5369c11f
ssethumadav <133330800+ssethumadav@users.noreply.github.com>
2025-02-20 12:55:28 +0530

    Update and rename jenkins_py_example to jenkins_py_example.py

--- jenkins_py_example.py
+++ jenkins_py_example.py
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Clone Repository') {
             steps {
-                git 'https://github.com/ssethumadav/DEVOPS-Training-Tasks.git'
+                git 'git branch: 'main', url: 'https://github.com/ssethumadav/DEVOPS-Training-Tasks.git''
             }
         }
         
